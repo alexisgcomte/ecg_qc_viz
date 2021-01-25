d3173afb491352dc9ab92075be203114386c88ee
alexisgcomte <alexis.g.comte@gmail.com>
2021-01-25 19:52:26 +0100

    adding spectrum frequency and scaleogram visualisation

--- streamlit_visualization/modules/graph_generation.py
+++ streamlit_visualization/modules/graph_generation.py
@@ -6,6 +6,8 @@ import math
 import numpy as np
 import scaleogram as scg
 import streamlit as st
+import matplotlib.pyplot as plt
+from scipy import signal
 
 
 def ecg_graph_generation(df: pd.DataFrame,
@@ -26,9 +28,17 @@ def ecg_graph_generation(df: pd.DataFrame,
 
     # ecg_qc preidction
     classif_ecg_qc_data = ecg_qc_predict(graph_df)
+
+    generate_spectral_analysis(ecg_data=graph_df['ecg_signal'].values,
+                               start=0,
+                               end=len(graph_df['ecg_signal'].values),
+                               fs=fs,
+                               max_freq_display=50)
+
     classif_ecg_qc_cnn_data = ecg_qc_predict_cnn(graph_df,
-                                                wavelet_generation=
-                                                wavelet_generation)
+                                                 wavelet_generation=
+                                                 wavelet_generation,
+                                                 time_window=2)
 
     # consolidation of data
     data = [data[0],
@@ -120,17 +130,15 @@ def annot_classification_correspondance(classif: int) -> int:
 
 
 def ecg_qc_predict_cnn(dataset: pd.DataFrame,
-                       wavelet_generation=False) -> pd.DataFrame:
+                       wavelet_generation: bool = False,
+                       time_window: int = 2,
+                       fs: int = 1000) -> pd.DataFrame:
 
     ecg_qc_test = ecg_qc(model_type='cnn')
-    time_window = 2
-    fs = 1000
 
     classif_ecg_qc_data = pd.DataFrame(
         range(0, dataset.shape[0]),
         columns=['classif'])
-
-    print(dataset.index[0])
 
     for ecg_signal_index in range(
             math.floor(dataset.shape[0]/(fs * time_window)) + 1):
@@ -143,36 +151,65 @@ def ecg_qc_predict_cnn(dataset: pd.DataFrame,
         signal_quality = annot_classification_correspondance(
             ecg_qc_test.get_signal_quality(ecg_data))
 
-        classif_ecg_qc_data.iloc[ecg_signal_index * fs * time_window:
-                                 ecg_signal_index * fs * time_window +
-                                 fs * time_window] = signal_quality
+        classif_ecg_qc_data.iloc[start:end] = signal_quality
 
         if wavelet_generation:
-
-            # choose default wavelet function
-            scg.set_default_wavelet('morl')
-
-            signal_length = 120
-            # range of scales to perform the transform
-            scales = scg.periods2scales(np.arange(1, signal_length+1))
-            # the scaleogram
-
-            sample_ecg = []
-            for element in range(int(round(len(ecg_data)/4,0))):
-                sample_ecg.append(np.mean(ecg_data[4*element:4*element+4]))
-
-            print(len(sample_ecg))
-
-            ax = scg.cws(ecg_data,
-                         scales=scales,
-                         figsize=(10, 4.0),
-                         coi=False,
-                         ylabel="Hz",
-                         xlabel="Time",
-                         title="scaleogram from frame {} to {}, quality:{}".
-                         format(start + dataset.index[0]/fs,
-                                end + dataset.index[0]/fs,
-                                signal_quality))
-            st.pyplot(ax.figure)
+            generate_spectral_analysis(ecg_data=ecg_data,
+                                       start=start,
+                                       end=end,
+                                       fs=fs,
+                                       max_freq_display=50)
 
     return classif_ecg_qc_data
+
+
+def generate_spectral_analysis(ecg_data: list,
+                               start: int,
+                               end: int,
+                               classif: str = 'NA',
+                               max_freq_display: int = 40,
+                               fs: int = 1000):
+
+    fig, (ax0, ax1, ax2) = plt.subplots(3, 1, figsize=(8, 4), sharex=True)
+    fig.subplots_adjust(hspace=.01)
+
+    # ax0
+    ax0.plot(np.arange(0, len(ecg_data)), ecg_data, linewidth=1)
+    ax0.axis('off')
+
+    #  ax2
+    f, t, Sxx = signal.spectrogram(ecg_data,
+                                   fs,
+                                   window=('tukey', .1),
+                                   nperseg=100)
+    ax1.pcolormesh(t*fs, -f, Sxx, shading='gouraud')
+    ax1.set_ylim(-max_freq_display)
+    ax1.set_ylabel('Hz (inv)')
+
+    # ax1
+    scg.set_default_wavelet('morl')
+
+    signal_length = max_freq_display
+    # range of scales to perform the transform
+    scales = scg.periods2scales(np.arange(1, signal_length+1))
+
+    # the scaleogram
+
+    sample_ecg = []
+    for element in range(int(round(len(ecg_data)/4, 0))):
+        sample_ecg.append(np.mean(ecg_data[4*element:4*element+4]))
+    scg.cws(ecg_data,
+            scales=scales,
+            figsize=(10, 2.0),
+            coi=False,
+            ylabel="Hz",
+            xlabel='Frame',
+            ax=ax2,
+            cbar=None,
+            title='')
+
+    fig.suptitle('spectrum frequency from frame {} to {} - classif : {}'.
+                 format(start, end, classif),
+                 fontsize=10)
+
+    st.pyplot(fig)
